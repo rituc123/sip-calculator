a57762d9edf92361305c08d32e498583f312cd9c
rituc123 <ritu373184@gmail.com>
2026-05-07 23:10:14 +0530

    Add calculator functions for FD, Lumpsum, and Retirement

--- script.js
+++ script.js
@@ -69,14 +69,90 @@ function toggleDarkMode(){
 
 function showCalculator(type){
 
+document.getElementById("sipBox").style.display = "none";
+
+document.getElementById("emiBox").style.display = "none";
+
+document.getElementById("fdBox").style.display = "none";
+
+document.getElementById("lumpsumBox").style.display = "none";
+
+document.getElementById("retirementBox").style.display = "none";
+
+
 if(type === 'sip'){
-  document.getElementById("sipBox").style.display = "block";
-  document.getElementById("emiBox").style.display = "none";
+document.getElementById("sipBox").style.display = "block";
+}
+
+else if(type === 'emi'){
+document.getElementById("emiBox").style.display = "block";
+}
+
+else if(type === 'fd'){
+document.getElementById("fdBox").style.display = "block";
+}
+
+else if(type === 'lumpsum'){
+document.getElementById("lumpsumBox").style.display = "block";
 }
 
 else{
-  document.getElementById("sipBox").style.display = "none";
-  document.getElementById("emiBox").style.display = "block";
+document.getElementById("retirementBox").style.display = "block";
 }
+
+}
+function calculateFD(){
+
+let P = document.getElementById("fdAmount").value;
+
+let r = document.getElementById("fdRate").value / 100;
+
+let t = document.getElementById("fdYears").value;
+
+let A = P * Math.pow((1 + r), t);
+
+document.getElementById("fdResult").innerHTML =
+"₹ " + Math.round(A);
+
+}
+
+
+
+function calculateLumpsum(){
+
+let P = document.getElementById("lumpAmount").value;
+
+let r = document.getElementById("lumpRate").value / 100;
+
+let t = document.getElementById("lumpYears").value;
+
+let A = P * Math.pow((1 + r), t);
+
+document.getElementById("lumpResult").innerHTML =
+"₹ " + Math.round(A);
+
+}
+
+
+
+function calculateRetirement(){
+
+let P = document.getElementById("retireMonthly").value;
+
+let annualRate =
+document.getElementById("retireRate").value;
+
+let years =
+document.getElementById("retireYears").value;
+
+let r = annualRate / 12 / 100;
+
+let n = years * 12;
+
+let corpus =
+P * (((Math.pow(1+r,n))-1)/r)*(1+r);
+
+document.getElementById("retireResult").innerHTML =
+"₹ " + Math.round(corpus);
 
 }
